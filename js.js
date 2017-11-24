7f87a32f36a857185a96c504fe0031696ef62a8d
榎本敦士 <acro1452@gmail.com>
2017-11-24 17:32:46 +0900

    dstcommit

---
+++ js.js
@@ -0,0 +1,53 @@
+
+function c() {
+
+    var input = $("#input").val();
+    var pointer = 0;
+
+//10 20 30 *+=
+    var in_arr = input.split(" ");
+    alert(in_arr);
+    console.log(in_arr);
+//{10 20 30 *+=}
+    var stack = [];
+    for (var i = 0; i < in_arr.length; i++) {
+        switch (in_arr[i]) {
+            case "+":
+                break;
+            case "-":
+                break;
+            case "*":
+                break;
+            case "/":
+                break;
+            default:
+                se(in_arr[i], i);
+                break;
+            anima();
+
+        }
+
+    }
+
+    function se(input, i) {
+        if (typeof(parseInt(input)) === 'number') {
+
+            $('input[name="[i]"]').innerHTML = i;
+        }
+
+        if (typeof(input) === 'string') {
+
+            $('input[name="[i]"]').innerHTML = i;
+        }
+    }
+
+
+}
+
+function anima() {
+   pointer = a;
+
+
+}
+
+
